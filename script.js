698a777afeb362df7b8daf06a3ca256efd7a8d97
Joyassroy <nadimbadsha4@gmail.com>
2025-08-23 00:34:30 +0600

    login details done

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // login button functionality
-document.getElementById('loginButton').addEventListener('click',function () {
-    console.log("login button clicked");
-});
+document.getElementById('loginButton').addEventListener('click',function (e) {
+    const mobileNumber=12345;
+    const pinNumber=1234;
+    const mobileNumberValue=document.getElementById('mobile-number').value;
+    const mobileNumberConverted=parseInt(mobileNumberValue);
+    const pinNumberValue=document.getElementById('pin-number').value;
+    const pinNumberConverted=parseInt(pinNumberValue);
+
+    if(mobileNumber==mobileNumberConverted && pinNumber==pinNumberConverted) {
+        window.location.href='./home.html';
+    }
+    else{
+        alert("Invalid Credentials");
+    }
+
+}); 
